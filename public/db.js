c81ed8edd906b03afaac1886deeb3cc074a3aae1
mssanoko <lawsonjleslie@gmail.com>
2020-10-09 22:59:26 -0400

    db.js file

--- public/db.js
+++ public/db.js
@@ -26,5 +26,28 @@ function saveRecord(record) {
     const store = transaction.objectStore("pending");
     store.add(record);
 }
-// listen for app coming back online
-window.addEventListener("online", checkDatabase)
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+          fetch("/api/transaction", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
+            }
+          })
+          .then(response => response.json())
+          .then(() => {
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+          });
+        }
+      };
+    }
+window.addEventListener("online", checkDatabase);
